a461f1ecec0ef9b5a6113adb04b3c4cb3a975444
MelissaOcampo <melissa.ocampo.2001@gmail.com>
2023-01-16 21:09:41 +0000

    List

---
+++ src/js/views/detalles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+import Perfil from "../component/perfil.js"
+
+export const Detalles = () => (
+	<>
+	
+		 <Perfil/>
+
+	</>
+);
